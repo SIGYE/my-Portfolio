2d4f3f0fb281c1e06f6938b1c205149e3944e36d
Mwesigye Linda <lindamwesigye2@gmail.com>
2023-09-26 08:29:22 +0200

    end of portfolio-part

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,17 +4,17 @@ import IMG1 from '../../assets/IMG1.png'
 import IMG2 from '../../assets/IMG2.png'
 import IMG3 from '../../assets/IMG3.png'
 
-const data = ()=>[
+const data = [
   {id: 1,
   image: IMG1,
-  title: 'Worker rating app dashboards',
+  title: 'Worker rating app; Akazi Kanoze; dashboards',
   github: 'https://github.com/SIGYE',
   demo: 'https://dribbble.com/Linda_teta'
   },
 
   {id: 2,
     image: IMG2,
-    title: 'LIve-stream dashboard for artsit Ariel Wayz',
+    title: 'Live-stream dashboard for artsit Ariel Wayz',
     github: 'https://github.com/SIGYE',
     demo: 'https://dribbble.com/Linda_teta'
     },
@@ -43,7 +43,7 @@ const Portfolio = () => {
             <div className='portfolio__item-image'>
               <img src={image} alt={title} height={200} width={250} />
             </div>
-              <h3>This is a portfolio item list</h3>
+              <h3>{title}</h3>
               <div className="portfolio__item-cta">
               <a href={github} className='btn' target='_blank'>Github</a>
               <a href={demo}className='btn btn-primary'>Live Demo</a>
