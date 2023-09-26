4aad25e0fe0e71524a47f201cd74c751defa2e81
Mwesigye Linda <lindamwesigye2@gmail.com>
2023-09-26 19:10:37 +0200

    contact-page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Let's Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className="contact__options">
+      <article className='contact__options'>
+        <MdOutlineEmail/>
+        <h4>Email</h4>
+        <h5>lindamwesigye2@gmail.com</h5>
+        <a href="mailto:lindamwesigye2@gmail.com">Send a message</a>
+      </article>
+        </div>
+        {/* ############# */}
+
+        <form action="">
+          
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -7,101 +7,74 @@ import Dzebraaa from '../../assets/Dzebraaa.jpeg'
 import Kelly  from '../../assets/Kelly.jpg'
 import liza from '../../assets/liza.png'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules'
+  // Import Swiper styles
+  import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+  avatar:lisa,
+  name: 'Lisa Tesi MWESIGYE',
+  review: 'Linda is quite a talented programmer basing on not only her speed but also the warmth she does her work with. Her main motive when working on a project is not to earn money from it but to make it successfull and viable. She is kind, entertaining and a social person. This boosts her markatability which priviledges her and the project she is working on. I look forward to work on many more projects with her'
+  },
+  {
+    avatar: Kelly,
+    name: 'Kelly MWESIGYE',
+    review: 'Linda is quite a talented programmer basing on not only her speed but also the warmth she does her work with. Her main motive when working on a project is not to earn money from it but to make it successfull and viable. She is kind, entertaining and a social person. This boosts her markatability which priviledges her and the project she is working on. I look forward to work on many more projects with her'
+    },
+    {
+      avatar: Bikeshya,
+      name: 'Accarie Davine Cyuzuzo',
+      review: 'Linda is quite a talented programmer basing on not only her speed but also the warmth she does her work with. Her main motive when working on a project is not to earn money from it but to make it successfull and viable. She is kind, entertaining and a social person. This boosts her markatability which priviledges her and the project she is working on. I look forward to work on many more projects with her'
+      },
+      {
+        avatar: liza,
+        name: 'Liza Lounge MBABAZI',
+        review: 'Linda is quite a talented programmer basing on not only her speed but also the warmth she does her work with. Her main motive when working on a project is not to earn money from it but to make it successfull and viable. She is kind, entertaining and a social person. This boosts her markatability which priviledges her and the project she is working on. I look forward to work on many more projects with her'
+        },
+        {
+          avatar: Dzebraaa,
+          name: 'Deborah Isimbi RUTAGENGWA',
+          review: 'Linda is quite a talented programmer basing on not only her speed but also the warmth she does her work with. Her main motive when working on a project is not to earn money from it but to make it successfull and viable. She is kind, entertaining and a social person. This boosts her markatability which priviledges her and the project she is working on. I look forward to work on many more projects with her'
+          },
+          {
+            avatar: aggy,
+            name: 'Agnes UWASE',
+            review: 'Linda is quite a talented programmer basing on not only her speed but also the warmth she does her work with. Her main motive when working on a project is not to earn money from it but to make it successfull and viable. She is kind, entertaining and a social person. This boosts her markatability which priviledges her and the project she is working on. I look forward to work on many more projects with her'
+            },
+]
+
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-
-      <div className='container testimonials__container'>
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={lisa} alt="pic one" width={70} height={80} />
-            </div>
-            <h5 className='client__name'>Lisa TESI MWESIGYE</h5>
-            <small className='client__review'>
-              Linda is quite a talented programmer basing on not only her speed but also the warmth she 
-              does her work with. Linda's main motive when working on a project is not to earn money from
-               it but to make it successfull and viable. She's kind, entertaining and a social person. This boosts
-               her markatability which priviledges her and the project she is working on. I look forward to work
-               on many more projects with her. 
-            </small>
-          
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={Kelly} alt="pic one" width={70} height={80} />
-            </div>
-            <h5 className='client__name'>Kelly MWESIGYE</h5>
-            <small className='client__review'>
-              Linda is quite a talented programmer basing on not only her speed but also the warmth she 
-              does her work with. Linda's main motive when working on a project is not to earn money from
-               it but to make it successfull and viable. She's kind, entertaining and a social person. This boosts
-               her markatability which priviledges her and the project she is working on. I look forward to work
-               on many more projects with her. 
-            </small>
-          
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={Bikeshya} alt="pic one" width={70} height={80} />
-            </div>
-            <h5 className='client__name'>Accarie DAVINE CYUZUZO</h5>
-            <small className='client__review'>
-              Linda is quite a talented programmer basing on not only her speed but also the warmth she 
-              does her work with. Linda's main motive when working on a project is not to earn money from
-               it but to make it successfull and viable. She's kind, entertaining and a social person. This boosts
-               her markatability which priviledges her and the project she is working on. I look forward to work
-               on many more projects with her. 
-            </small>
-          
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={liza} alt="pic one" width={70} height={80} />
-            </div>
-            <h5 className='client__name'>Liza LOUNGE MBABAZI</h5>
-            <small className='client__review'>
-              Linda is quite a talented programmer basing on not only her speed but also the warmth she 
-              does her work with. Linda's main motive when working on a project is not to earn money from
-               it but to make it successfull and viable. She's kind, entertaining and a social person. This boosts
-               her markatability which priviledges her and the project she is working on. I look forward to work
-               on many more projects with her. 
-            </small>
-          
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={Dzebraaa} alt="pic one" width={70} height={80} />
-            </div>
-            <h5 className='client__name'>Deborah RUTAGENGWA</h5>
-            <small className='client__review'>
-              Linda is quite a talented programmer basing on not only her speed but also the warmth she 
-              does her work with. Linda's main motive when working on a project is not to earn money from
-               it but to make it successfull and viable. She's kind, entertaining and a social person. This boosts
-               her markatability which priviledges her and the project she is working on. I look forward to work
-               on many more projects with her. 
-            </small>
-          
-        </article>
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={aggy} alt="pic one" width={70} height={80} />
-            </div>
-            <h5 className='client__name'>Agnes UWASE</h5>
-            <small className='client__review'>
-              Linda is quite a talented programmer basing on not only her speed but also the warmth she 
-              does her work with. Linda's main motive when working on a project is not to earn money from
-               it but to make it successfull and viable. She's kind, entertaining and a social person. This boosts
-               her markatability which priviledges her and the project she is working on. I look forward to work
-               on many more projects with her. 
-            </small>
-          
-        </article>
-      </div>
+        <h5>Review from clients</h5>
+        <h2>Testimonials</h2>
+        
+      <Swiper className='container testimonials__container'
+        modules={[Pagination]} spacebetween={40}
+        slidesPerView={1}
+        pagination={{clickable : true}}>
+    {
+      data.map(({avatar, name, review}, index)=>{
+        return(
+        <SwiperSlide key={index} className='testimonial'>
+        <div className="client__avatar">
+          <img src={avatar} width={70} height={80} />
+          </div>
+          <h5 className='client__name'>{name}</h5>
+          <small className='client__review'>
+          {review}
+          </small>
+        
+      </SwiperSlide>
+        )
+      })
+    }
+      </Swiper>
     </section>
   )
 }
